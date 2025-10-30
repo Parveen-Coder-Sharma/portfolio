08ac4f9d7ef4e40b05247c7d69349ffa6560580f
Parveen-Coder-Sharma <parveendecentsharma@gmail.com>
2025-10-30 16:13:57 +0530

    Initial portfolio commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.style.background = "#111";
+  } else {
+    navbar.style.background = "#212529";
+  }
+});
